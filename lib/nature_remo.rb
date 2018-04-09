adc508d8c469685c3a116088ac1170cf1eb6e86a
ichi-t <taisuke_ichimura@icloud.com>
2018-04-09 21:18:07 +0900

    add devices method in cli

--- lib/nature_remo.rb
+++ lib/nature_remo.rb
@@ -1,5 +1,6 @@
 require "nature_remo/version"
 require "nature_remo/client"
+require "nature_remo/cli"
 
 module NatureRemo
   # Your code goes here...
--- lib/nature_remo/cli.rb
+++ lib/nature_remo/cli.rb
@@ -1,12 +1,14 @@
 require 'thor'
+require 'json'
 
-module NatureRemo < Thor
-  class Cli
+module NatureRemo
+  class Cli < Thor
     desc 'devices', 'find all devices'
     def devices
-      client.devices
+      p JSON.parse(client.devices.body)
     end
 
+    private
     def client
       @client ||= NatureRemo::Client.new
     end
